62f870f63edf59f59d27df384f5ad52061fbd75b
ashok <ashoktandan@outlook.com>
2019-04-07 18:23:32 +0530

    sssl

--- server_example/server_ssl.js
+++ server_example/server_ssl.js
@@ -3,7 +3,10 @@ var https   = require("https");     // https server core module
 var fs      = require("fs");        // file system core module
 var express = require("express");   // web framework external module
 var io      = require("socket.io"); // web socket external module
-
+var fs=require('fs')
+var path=require('path')
+var multer=require('multer')
+var unirest = require("unirest");
 // This sample is using the easyrtc from parent folder.
 // To use this server_example folder only without parent folder:
 // 1. you need to replace this "require("../");" by "require("easyrtc");"
@@ -14,7 +17,55 @@ var easyrtc = require("../"); // EasyRTC internal module
 // Setup and configure Express http server. Expect a subfolder called "static" to be the web root.
 var httpApp = express();
 httpApp.use(express.static(__dirname + "/static/"));
+var reqURL = unirest("GET", "https://www.fast2sms.com/dev/bulk");
+httpApp.post('/sendsms',function(req,res){
+    reqURL.query({
+        "authorization": "KiQ3ZX2IkyY18w0N4WufFp6rBzqhTmvED9GxRsHebMV5nolAJLMidkyIf94tjsZ7bB6wzXrGTQClgU3D",
+        "sender_id": "FSTSMS",
+        "message": "https://157.230.171.151:8443/demos/demo_audio_video_simple.html",
+        "language": "english",
+        "route": "p",
+        "numbers": "8802985527",
+      });
+      
+      reqURL.headers({
+        "cache-control": "no-cache"
+      });
+    
+    reqURL.end(function (resp) {
+        if (resp.error){
+            res.end('error')
+        } 
+      });
+      res.end('success')
+});
 
+
+httpApp.post('/blob',function(req,res){
+    var storage = multer.diskStorage({
+        destination: __dirname+'/videos',
+        filename: function (req, file, cb) {
+            cb(null, Date.now() + '.webm') //Appending .jpg
+          }
+    });
+    var upload = multer({
+        storage: storage
+    }).any();
+
+    upload(req, res, function(err) {
+        if (err) {
+            console.log(err);
+            return res.end('Error');
+        } else {
+            console.log(req.body);
+            req.files.forEach(function(item) {
+                console.log(item);
+                // move your file to destination
+            });
+            res.end('File uploaded');
+        }
+    }); 
+})
 // Start Express https server on port 8443
 var webServer = https.createServer({
     key:  fs.readFileSync(__dirname + "/certs/localhost.key"),
